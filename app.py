4983dab246302c5e3398d223a8cdaf1202bf7d49
KeloDraken <joymama078@gmail.com>
2022-05-14 21:55:38 +0200

    Basic styling works

--- app.py
+++ app.py
@@ -6,12 +6,16 @@ app = Aquilo("KeloDraken", "The web framework for pythoneers")
 
 @app.route("/")
 def hello():
+    h1_styles = {"color": "pink"}
+    ht = {"font-size": "100px"}
+
     elements = [
-        h1("Welcome to Aquilo"),
+        h1("Welcome to Aquilo, by Samkelo Drakenberg", ["h1_styles", "ht"]),
         h2("The web framework for pythoneers"),
         p("This was generated using python")
     ]
-    app.register(elements)
+    app.register_elements(elements)
+    app.register_styles("h1_styles", [h1_styles, ht])
 
 
 if __name__ == "__main__":
