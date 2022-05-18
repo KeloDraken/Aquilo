fca6ae0b61ce2c2e856288040dd1cec4af985199
KeloDraken <joymama078@gmail.com>
2022-05-18 14:39:24 +0200

    Add default home page and 404 page

--- aquilo/browser/elements/containers/div.py
+++ aquilo/browser/elements/containers/div.py
@@ -6,7 +6,11 @@ from aquilo.ui.styles import StyleSheet
 
 
 class div(Element):
-    def __init__(self, *args: Union[Iterable, Element], styles: StyleSheet = StyleSheet.create({})):
+    def __init__(
+        self,
+        *args: Union[Iterable, Element],
+        styles: StyleSheet = StyleSheet.create({})
+    ):
         self.styles = styles
         etype = self.__class__.__name__
         super().__init__(etype)
