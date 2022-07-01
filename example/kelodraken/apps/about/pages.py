a51b48f8a184245ac306c996be72a028238c7be5
Joy Mama-Kgosiemang <jmama021@student.wethinkcode.co.za>
2022-07-01 23:33:34 +0200

    remove example folder

--- example/kelodraken/apps/about/pages.py
+++
@@ -1,6 +0,0 @@
-from src.aquilo import div, h1, build
-
-
-def page_about():
-    root = div(h1(text="this is the about page"))
-    return build(root, title="This is the about page")
--- example/kelodraken/apps/contact/pages.py
+++
@@ -1,6 +0,0 @@
-from src.aquilo import div, build, a
-
-
-def page_contact():
-    root = div(a("home", "/home/"))
-    return build(root, title="Contact Us")
