f789887f437b3608c4fd80818f5f72161fbfc73c
vuyo <joymama078@gmail.com>
2023-03-09 09:54:56 +0200

    create section element

---
+++ aquilo/browser/elements/containers/section.py
@@ -0,0 +1,21 @@
+from typing import Union, Iterable, List
+
+from aquilo.browser.elements import Element
+from aquilo.html.generators import generate_dom_tree
+
+
+class section(Element):
+    def __init__(
+        self,
+        *args: Union[Iterable, Element],
+        class_list: List[str] = None
+    ):
+        etype = self.__class__.__name__
+        super().__init__(etype, class_list=class_list)
+        self.elements = args
+
+    def get_elements(self):
+        return self.elements
+
+    def __call__(self, *args, **kwargs):
+        return generate_dom_tree(self.elements)
